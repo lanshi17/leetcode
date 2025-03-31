6828e075a3672b2af4b44a42fe3b59142bee046d
lanshi <yzs20030317@gmail.com>
2025-03-31 21:54:36 +0800

    fix(remove-nth-node): 修复删除倒数第n个节点函数
    - 添加虚拟头节点以简化边界情况处理
    - 使用双指针法找到倒数第n个节点的前一个节点
    - 删除目标节点并返回更新后的链表头节点

--- remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list.cpp
@@ -19,7 +19,21 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
+       ListNode *dummy =new ListNode(-1); 
+       dummy->next=head;
+       ListNode *p1=dummy;
+       ListNode *p2=dummy;
+       for (int i = 0; i <= n; ++i) {
+         p1=p1->next;
+       } 
+       while (p1!=nullptr) {
+         p1=p1->next;
+         p2=p2->next;
+       }
+       ListNode *tmp =p2->next;
+       p2->next=p2->next->next;
+       delete tmp;
+       return dummy->next;
     }
 };
 // @lc code=end
