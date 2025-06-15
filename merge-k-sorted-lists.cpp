6286e26d4aaac289814b161c2b253974403dede4
lanshi <yzs20030317@gmail.com>
2025-06-15 21:23:20 +0800

    feat: 优化合并 K 个升序链表的实现，使用分治法替代优先队列

--- merge-k-sorted-lists.cpp
+++ merge-k-sorted-lists.cpp
@@ -22,27 +22,38 @@ public:
         if (lists.empty()) {
           return nullptr;
         }
-        ListNode *dummy=new ListNode(-1);
-        ListNode *p=dummy;
-        auto cmp=[](ListNode *a,ListNode *b){
-            return a->val>b->val;
-        };
-        priority_queue<ListNode*,vector<ListNode*>,decltype(cmp)> pq(cmp);
-        for (const auto& head : lists) {
-          if (head!=nullptr) {
-            pq.push(head);
-          }
+        return mergeKLists3(lists,0,lists.size()-1); 
+    }
+    ListNode* mergeKLists3(vector<ListNode*>& lists,int start,int end){
+      if (start==end) {
+        return lists[start];
+      }
+      int mid=start+(end-start)/2;
+      ListNode* left =mergeKLists3(lists,start,mid);
+      ListNode* right=mergeKLists3(lists,mid+1,end);
+      return mergeKTwoLists(left,right);
+    }
+    ListNode* mergeKTwoLists(ListNode* l1,ListNode* l2){
+      ListNode dummy(-1),*p=&dummy;
+      ListNode*p1=l1,*p2=l2;
+      while (p1!=nullptr&&p2!=nullptr) {
+        if (p1->val>p2->val) {
+          p->next=p2;
+          p2=p2->next;
+        }else
+        {
+          p->next=p1;
+          p1=p1->next;
         }
-        while (!pq.empty()) {
-          ListNode *node=pq.top();
-          pq.pop();
-          p->next=node;
-          if (node->next!=nullptr) {
-            pq.push(node->next);
-          }
-          p=p->next;
-        }
-        return dummy->next; 
+        p=p->next;
+      }
+      if (p1!=nullptr) {
+        p->next=p1;
+      }
+      if (p2!=nullptr) {
+        p->next=p2;
+      }
+      return dummy.next;
     }
 };
 // @lc code=end
